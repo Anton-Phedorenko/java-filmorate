7084c05c88acff0ea3764bde86de0ef0c87d7111
Anton <anton.phedornko.01@yandex.ru>
2022-08-09 17:20:41 +0300

    убрал закоментированный код

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,57 +14,53 @@ import java.util.Map;
 
 @RestController
 public class FilmController {
-    Logger log=LoggerFactory.getLogger(FilmController.class);
+    Logger log = LoggerFactory.getLogger(FilmController.class);
     Map<Integer, Film> films = new HashMap<>();
     private int filmId;
+
     @GetMapping("/films")
     public List<Film> getFilms() {
         return new ArrayList<>(films.values());
     }
+
     @PostMapping(value = "/films")
-    public Film createFilm(@RequestBody Film film){
-        if(validateFilm(film)){
+    public Film createFilm(@RequestBody Film film) {
+        if (validateFilm(film)) {
             film.setId(generateId());
-            films.put(film.getId(),film);
-            log.debug("Фильм "+film.getName()+" успешно добавлен");
+            films.put(film.getId(), film);
+            log.debug("Фильм " + film.getName() + " успешно добавлен");
             return film;
-        }
-        else{
+        } else {
             throw new ValidationException("Ошибка валидации при попытке добавить фильм");
         }
     }
+
     @PutMapping(value = "/films")
-    public Film updateFilm(@RequestBody Film film){
-        if(validateFilm(film)&&films.containsKey(film.getId())){
-            films.put(film.getId(),film);
+    public Film updateFilm(@RequestBody Film film) {
+        if (validateFilm(film) && films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
             log.debug("Фильм обновлен");
             return film;
-        }
-        else {
+        } else {
             throw new ValidationException("Ошибка при попытке обновить фильм");
         }
     }
-    public boolean validateFilm(Film film){
-//        if(!film.getName().isEmpty()&&film.getDescription().length()<200&&
-//                film.getReleaseDate().isAfter(LocalDate.of(1895,12,28))&&film.getDuration()>0){
-//            return true;
-//        }
-//        return false;
-        if(film.getName().isEmpty()){
+
+    public boolean validateFilm(Film film) {
+//
+        if (film.getName().isEmpty()) {
             throw new ValidationException("У фильма должно быть название");
-        }
-        else if(film.getDescription().length()>200){
+        } else if (film.getDescription().length() > 200) {
             throw new ValidationException("Слишком длинное описание фильма");
-        }
-        else if(!film.getReleaseDate().isAfter(LocalDate.of(1895,12,28))){
+        } else if (!film.getReleaseDate().isAfter(LocalDate.of(1895, 12, 28))) {
             throw new ValidationException("В то время вряд ли могли выпустить фльм:(");
-        }
-        else if(film.getDuration()<0){
+        } else if (film.getDuration() < 0) {
             throw new ValidationException("Длительность фильма не может быть отрицательной");
         }
         return true;
     }
-    private int generateId(){
+
+    private int generateId() {
         this.filmId++;
         return this.filmId;
     }
